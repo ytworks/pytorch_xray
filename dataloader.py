ed018e4fd1aef310b0838ae7295275f548a0f9af
ytworks <yuzo.takagi@gmail.com>
2018-12-22 11:57:30 +0900

    plan

---
+++ dataloader.py
@@ -0,0 +1,7 @@
+
+
+
+# 1. transform
+# 2. dataset_class
+# 3. dataloader
+# 4. to train.py
---
+++ train.py
@@ -0,0 +1,13 @@
+
+# -2. configの設定
+# -1. cudaの設定
+# 0. Modelの設定　(from inference)
+# 0.5. dataloader
+# 1. Lossの設定
+# 2. preprocess
+# 3. Loss(pred, true)
+# 4. no grad
+# 5. backward
+# 6. opt
+# 7. eval
+# save
