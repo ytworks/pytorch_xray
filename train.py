563337adffa20d4e74ae47e9793319caa2c0c46d
ytworks <yuzo.takagi@gmail.com>
2018-12-23 16:54:23 +0900

    cuda

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 import inference
+import os
 
-# -1. cudaの設定
 # 0.5. dataloader
 # 1. Lossの設定
 # 2. preprocess
@@ -22,7 +22,14 @@ def main():
     # Configの読み込み (utils)
     ini, debug_mode = utils.config.read_config()
     print("Debug mode:", debug_mode)
+    # model
     model = inference.Model(model_name='densenet121', pretrained=True, pooling='max')
+    # cuda
+    num_gpu = ini.getint('env', 'num_gpu')
+    if num_gpu > 1:
+        model = nn.DataParallel(model)
+    if num_gpu > 0:
+        model.cuda()
 
 
 
