9cb76955b680bf78840c03ddce41fa5ef2d8486a
ytworks <yuzo.takagi@gmail.com>
2018-12-30 13:10:03 +0900

    init

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_cosine.ini
 
 CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_balance.ini
 CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_balance.ini
+
+CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_custom.ini
+CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_custom.ini
