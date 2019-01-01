1abab77e597d4389ca54af5bcbe10742050b0607
ytworks <yuzo.takagi@gmail.com>
2019-01-01 21:14:56 +0900

    sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_stepLR.ini
-CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_stepLR.ini
+
 
 CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_cosine.ini
 CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_cosine.ini
+
+CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_15.ini
+CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_15.ini
 
 CUDA_VISIBLE_DEVICES=0 python train.py -config ./experiments/base_balance.ini
 CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./experiments/base_balance.ini
