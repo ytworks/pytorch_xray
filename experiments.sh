92f9fc560d221b15dc70e3c1b5e891f17a5fc4de
ytworks <yuzo.takagi@gmail.com>
2019-01-02 22:35:08 +0900

    experiments/1

---
+++ experiments.sh
@@ -0,0 +1,8 @@
+python train.py -config ./base.ini
+python evaluate.py -config ./base.ini
+
+python train.py -config ./base_15.ini
+python evaluate.py -config ./base_15.ini
+
+python train.py -config ./custom.ini
+python evaluate.py -config ./custom.ini
