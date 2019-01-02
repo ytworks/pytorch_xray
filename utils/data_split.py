acb697f32305ac903cc413f9aece2aa45d66bfb4
ytworks <yuzo.takagi@gmail.com>
2019-01-02 13:14:20 +0900

    define

--- utils/data_split.py
+++ utils/data_split.py
@@ -5,9 +5,9 @@ import pandas as pd
 def data_split(ini, labels, debug_mode):
     # TBD patient-wise
     tv_list = [x[0] for x in pd.read_csv(
-        ini.get('data', 'official_train'), usecols=[0]).values]
+        ini.get('data', 'train_path'), usecols=[0]).values]
     test_list = [x[0] for x in pd.read_csv(
-        ini.get('data', 'official_test'), usecols=[0]).values]
+        ini.get('data', 'test_path'), usecols=[0]).values]
     val_ratio = ini.getfloat('params', 'validation_ratio')
     train_ratio = ini.getfloat('params', 'train_ratio')
     test_ratio = ini.getfloat('params', 'test_ratio')
@@ -16,9 +16,9 @@ def data_split(ini, labels, debug_mode):
         tv_list, test_size=ratio, random_state=None)
     assert len(train_list) + len(val_list) == len(tv_list), "Split Error"
     if debug_mode:
-        train_list = train_list[0:300]
-        val_list = val_list[0:300]
-        test_list = test_list[0:300]
+        train_list = train_list[0:32]
+        val_list = val_list[0:32]
+        test_list = test_list[0:32]
     print("train, validation, test = ", len(
         train_list), len(val_list), len(test_list))
     return train_list, val_list, test_list
