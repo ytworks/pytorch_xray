2bad77f8b3674234d473e2cfb9a1b9b24712effc
ytworks <test@test.com>
2019-01-07 11:07:42 +0900

    sh

--- experiments.sh
+++ experiments.sh
@@ -1,2 +1,2 @@
-python train.py -config ./dropout.ini
-python evaluate.py -config ./dropout.ini
+CUDA_VISIBLE_DEVICES=0,1,2 python train.py -config ./dropout.ini
+CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./dropout.ini
