9d9c916aad1d6613f5d5cbfed7e78fd893db9958
ytworks <test@test.com>
2019-01-09 13:18:30 +0900

    transfer mode

--- train.py
+++ train.py
@@ -80,10 +80,14 @@ def main():
         ckpt_path=ini.get('model', 'restore_path')
         ckpt = torch.load(ckpt_path, map_location=device)
         pretrained_dict = {}
-        for k, v in ckpt['state_dict'].items():
-            if k.find('features') > -1:
-                pretrained_dict.setdefault(k, v)
-        model.load_state_dict(pretrained_dict, strict=False)
+        if ini.getboolean('model', 'transfer_mode'):
+            for k, v in ckpt['state_dict'].items():
+                if k.find('features') > -1:
+                    pretrained_dict.setdefault(k, v)
+            model.load_state_dict(pretrained_dict, strict=False)
+        else:
+            model.load_state_dict(ckpt['state_dict'], strict=True)
+
     # cuda対応
     num_gpu = ini.getint('env', 'num_gpu')
     if num_gpu > 1:
