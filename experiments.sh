2df6dae9458a7758d0c6f936745f02df4200aa57
ytworks <yuzo.takagi@gmail.com>
2019-04-17 21:26:50 +0900

    generative model

--- experiments.sh
+++ experiments.sh
@@ -1,2 +1,2 @@
-python train.py -config ./generative_model.ini --d
-python evaluate.py -config ./generative_model.ini --d
+python train.py -config ./generative_model.ini
+python evaluate.py -config ./generative_model.ini
