446f348d2e3105a60caad003602df671a812d086
ytworks <yuzo.takagi@gmail.com>
2019-04-18 08:54:03 +0900

    original VAE

--- utils/loss.py
+++ utils/loss.py
@@ -34,11 +34,11 @@ class VAELoss(nn.Module):
         super(VAELoss, self).__init__()
 
     def forward(self, recon_x, x, mu, logvar):
-        #BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
-        MSE = F.mse_loss(recon_x, x, reduction='mean')
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        #MSE = F.mse_loss(recon_x, x, reduction='mean')
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        return MSE + KLD
+        return BCE + KLD
