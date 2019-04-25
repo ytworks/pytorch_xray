34dc8d854fee07ab6dc62ab94bc7d004841b04af
ytworks <yuzo.takagi@gmail.com>
2019-04-25 15:03:05 +0900

    aacn

--- utils/custom.py
+++ utils/custom.py
@@ -146,6 +146,124 @@ class SqEx(nn.Module):
         y = x * y
         return y
 
+class AugmentedConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dk, dv, Nh, relative):
+        super(AugmentedConv, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.dk = dk
+        self.dv = dv
+        self.Nh = Nh
+        self.relative = relative
+
+        self.conv_out = nn.Conv2d(self.in_channels, self.out_channels - self.dv, self.kernel_size, padding=1)
+
+        self.qkv_conv = nn.Conv2d(self.in_channels, 2 * self.dk + self.dv, kernel_size=1)
+
+        self.attn_out = nn.Conv2d(self.dv, self.dv, 1)
+
+    def forward(self, x):
+        # Input x
+        # (batch_size, channels, height, width)
+        batch, _, height, width = x.size()
+
+        # conv_out
+        # (batch_size, out_channels, height, width)
+        conv_out = self.conv_out(x)
+
+        # flat_q, flat_k, flat_v
+        # (batch_size, Nh, height * width, dvh or dkh)
+        # dvh = dv / Nh, dkh = dk / Nh
+        # q, k, v
+        # (batch_size, Nh, height, width, dv or dk)
+        flat_q, flat_k, flat_v, q, k, v = self.compute_flat_qkv(x, self.dk, self.dv, self.Nh)
+        logits = torch.matmul(flat_q.transpose(2, 3), flat_k)
+
+        if self.relative:
+            h_rel_logits, w_rel_logits = self.relative_logits(q)
+            logits += h_rel_logits
+            logits += w_rel_logits
+        weights = F.softmax(logits, dim=-1)
+
+        # attn_out
+        # (batch, Nh, height * width, dvh)
+        attn_out = torch.matmul(weights, flat_v.transpose(2, 3))
+        attn_out = torch.reshape(attn_out, (batch, self.Nh, self.dv // self.Nh, height, width))
+        # combine_heads_2d
+        # (batch, out_channels, height, width)
+        attn_out = self.combine_heads_2d(attn_out)
+        attn_out = self.attn_out(attn_out)
+        return torch.cat((conv_out, attn_out), dim=1)
+
+    def compute_flat_qkv(self, x, dk, dv, Nh):
+        N, _, H, W = x.size()
+        qkv = self.qkv_conv(x)
+        q, k, v = torch.split(qkv, [dk, dk, dv], dim=1)
+        q = self.split_heads_2d(q, Nh)
+        k = self.split_heads_2d(k, Nh)
+        v = self.split_heads_2d(v, Nh)
+
+        dkh = dk // Nh
+        q *= dkh ** -0.5
+        flat_q = torch.reshape(q, (N, Nh, dk // Nh, H * W))
+        flat_k = torch.reshape(k, (N, Nh, dk // Nh, H * W))
+        flat_v = torch.reshape(v, (N, Nh, dv // Nh, H * W))
+        return flat_q, flat_k, flat_v, q, k, v
+
+    def split_heads_2d(self, x, Nh):
+        batch, channels, height, width = x.size()
+        ret_shape = (batch, Nh, channels // Nh, height, width)
+        split = torch.reshape(x, ret_shape)
+        return split
+
+    def combine_heads_2d(self, x):
+        batch, Nh, dv, H, W = x.size()
+        ret_shape = (batch, Nh * dv, H, W)
+        return torch.reshape(x, ret_shape)
+
+    def relative_logits(self, q):
+        B, Nh, dk, H, W = q.size()
+        q = torch.transpose(q, 2, 4)
+
+        key_rel_w = nn.Parameter(torch.randn((2 * W - 1, dk), requires_grad=True))
+        rel_logits_w = self.relative_logits_1d(q, key_rel_w, H, W, Nh, "w")
+
+        key_rel_h = nn.Parameter(torch.randn((2 * H - 1, dk), requires_grad=True))
+        rel_logits_h = self.relative_logits_1d(torch.transpose(q, 2, 3), key_rel_h, W, H, Nh, "h")
+
+        return rel_logits_h, rel_logits_w
+
+    def relative_logits_1d(self, q, rel_k, H, W, Nh, case):
+        rel_logits = torch.einsum('bhxyd,md->bhmxy', q, rel_k)
+        rel_logits = torch.reshape(rel_logits, (-1, Nh * H, W, 2 * W - 1))
+        rel_logits = self.rel_to_abs(rel_logits)
+
+        rel_logits = torch.reshape(rel_logits, (-1, Nh, H, W, W))
+        rel_logits = torch.unsqueeze(rel_logits, dim=3)
+        rel_logits = rel_logits.repeat((1, 1, 1, H, 1, 1))
+
+        if case is "w":
+            rel_logits = torch.transpose(rel_logits, 3, 4)
+        elif case is "h":
+            rel_logits = torch.transpose(rel_logits, 2, 4).transpose(4, 5).transpose(3, 5)
+        rel_logits = torch.reshape(rel_logits, (-1, Nh, H * W, H * W))
+        return rel_logits
+
+    def rel_to_abs(self, x):
+        B, Nh, L, _ = x.shape
+
+        col_pad = torch.zeros((B, Nh, L, 1))
+        x = torch.cat((x, col_pad), dim=3)
+
+        flat_x = torch.reshape(x, (B, Nh, L * 2 * L))
+        flat_pad = torch.zeros((B, Nh, L - 1))
+        flat_x_padded = torch.cat((flat_x, flat_pad), dim=2)
+
+        final_x = torch.reshape(flat_x_padded, (B, Nh, L + 1, 2 * L - 1))
+        final_x = final_x[:, :, :L, L - 1:]
+        return final_x
+
 
 class Model_CUSTOM(nn.Module):
     def __init__(self, model_name, pretrained, kmax=1, kmin=None, alpha=1, num_maps=1, num_classes=15,
@@ -153,9 +271,9 @@ class Model_CUSTOM(nn.Module):
         super().__init__()
         model = models.densenet121(pretrained=pretrained)
         set_parameter_requires_grad(model, fine_tuning)
-        self.se1 = CBAM(128)
-        self.se2 = CBAM(256)
-        self.se3 = CBAM(512)
+        self.attention1 = AugmentedConv(in_channels=128, out_channels=128, kernel_size=3, dk=32, dv=64, Nh=8, relative=True)
+        self.attention2 = AugmentedConv(in_channels=256, out_channels=256, kernel_size=3, dk=64, dv=128, Nh=8, relative=True)
+        self.attention3 = AugmentedConv(in_channels=512, out_channels=512, kernel_size=3, dk=128, dv=256, Nh=8, relative=True)
         self.features = nn.Sequential()
         self.features.add_module('conv0', model.features.conv0)
         self.features.add_module('norm0', model.features.norm0)
@@ -163,13 +281,13 @@ class Model_CUSTOM(nn.Module):
         self.features.add_module('pool0', model.features.pool0)
         self.features.add_module('denseblock1', model.features.denseblock1)
         self.features.add_module('transition1', model.features.transition1)
-        self.features.add_module('se1', self.se1)
+        self.features.add_module('attention1', self.attention1)
         self.features.add_module('denseblock2', model.features.denseblock2)
         self.features.add_module('transition2', model.features.transition2)
-        self.features.add_module('se2', self.se2)
+        self.features.add_module('attention2', self.attention2)
         self.features.add_module('denseblock3', model.features.denseblock3)
         self.features.add_module('transition3', model.features.transition3)
-        self.features.add_module('se3', self.se3)
+        self.features.add_module('attention3', self.attention3)
         self.features.add_module('denseblock4', model.features.denseblock4)
         self.features.add_module('norm5', model.features.norm5)
         print(self.features)
