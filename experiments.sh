2023faa2c40dfc843404c19b53ecb108b5b10f5c
ytworks <yuzo.takagi@gmail.com>
2019-04-26 11:11:50 +0900

    fix

--- experiments.sh
+++ experiments.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py -config ./aacn.ini --d
-CUDA_VISIBLE_DEVICES=0,1,2,3 python evaluate.py -config ./aacn.ini --d
+CUDA_VISIBLE_DEVICES=0 python train.py -config ./aacn.ini
+CUDA_VISIBLE_DEVICES=0 python evaluate.py -config ./aacn.ini
